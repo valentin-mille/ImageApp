1bfc9aa19b6be3dacc00dd7639dfcd3000ff1c40
Mille <valentin.mille@epitech.eu>
2022-07-10 09:06:40 +0200

    feat(project): initialize project

---
+++ ImageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageApp
+//
+//  Created by Valentin Mille on 7/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
